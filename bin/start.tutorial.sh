82412e3b0aac24f4545fd20202754189fd1b9a01
lijunwei <lijunwei@udesk.cn>
2021-05-20 14:08:42 +0800

    Added note.

---
+++ bin/start.tutorial.sh
@@ -0,0 +1,9 @@
+
+# ref:
+# https://github.com/docker/getting-started
+# git clone git@github.com:docker/getting-started.git
+
+# ref
+# https://docs.docker.com/get-started/
+
+docker run --rm -d -p 80:80 docker/getting-started
