420dfa54499a75c89c349a936334a7cb6d8256a2
lijunwei <lijunwei@udesk.cn>
2021-05-20 16:47:36 +0800

    Added note.

--- bin/connect.mysql.sh
+++ bin/connect.mysql.sh
@@ -1,1 +1,4 @@
-docker exec -it $(docker ps|egrep 'mysql5.7.todo|mysql:5.7.34' | awk '{print $1}') mysql -uroot -p123456 -Dtodos
+echo "Usage:"
+echo "docker exec -it $(docker ps|egrep 'mysql5.7.todo|mysql:5.7.34' | awk '{print $1}') mysql -uroot -p123456 -Dtodos"
+
+docker exec -it $(docker ps|egrep 'mysql5.7.todo|mysql:5.7.34' | awk '{print $1}') mysql -uroot -p123456 -Dtodos -e "select * from todo_items;"
