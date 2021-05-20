a432d05544f7cf79c5f6461a3d1a7df5168cab56
lijunwei <lijunwei@udesk.cn>
2021-05-20 14:10:38 +0800

    Added note.

--- bin/start.tutorial.sh
+++ bin/start.tutorial.sh
@@ -6,4 +6,6 @@
 # ref
 # https://docs.docker.com/get-started/
 
-docker run --rm --name docker-getting-started -d -p 80:80 docker/getting-started
+docker run --rm --name docker-getting-started -d -p 80:80 docker/getting-started;
+
+docker ps;
